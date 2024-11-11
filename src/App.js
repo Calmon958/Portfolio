7ea0ef084d8d0243e17a5cf5a0a7c908ad67a957
wnjuguna <wm453856@gmail.com>
2024-11-11 22:38:11 +0300

    feat: Modify to accomodate for projects, skills, contact and hero tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Skills from './Skills';
+import Contact from './Contact';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Hero />
+            <About />
+            <Projects />
+            <Skills />
+            <Contact />
+            {/* Other components like About, Projects, etc., go here */}
+        </div>
+    );
 }
 
 export default App;
